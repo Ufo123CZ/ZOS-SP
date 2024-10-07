7b8462845378876247703d8802cd3ea5f418f449
Ufo123CZ <ufo123hry@seznam.cz>
2024-10-07 10:19:26 +0200

    Project init

---
+++ File_System/cmake-build-debug/CMakeFiles/File_System.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File_System.dir/main.cpp.o"
+  "CMakeFiles/File_System.dir/main.cpp.o.d"
+  "File_System"
+  "File_System.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/File_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
